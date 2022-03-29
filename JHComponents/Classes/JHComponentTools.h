de8769225b1198b53dd6877ae9833f53d459edf9
jade_xiaohui <sjh86126@yeah.net>
2022-03-29 17:14:05 +0800

    组件工具
    添加组件工具类

---
+++ JHComponents/Classes/JHComponentTools.h
@@ -0,0 +1,18 @@
+//
+//  JHComponentTools.h
+//  JHComponents
+//
+//  Created by jinhui song on 2022/3/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JHComponentTools : NSObject
+
+- (void)printJHComponentTools;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JHComponents/Classes/JHComponentTools.m
@@ -0,0 +1,15 @@
+//
+//  JHComponentTools.m
+//  JHComponents
+//
+//  Created by jinhui song on 2022/3/29.
+//
+
+#import "JHComponentTools.h"
+
+@implementation JHComponentTools
+
+- (void)printJHComponentTools {
+    NSLog(@"printJHComponentTools****");
+}
+@end
